5fbe24b64c2e024b5aa608ff0f9d9b7e3c4d540c
Marina P <petrova.dll@gmail.com>
2016-10-24 11:09:21 +0200

    Pass score instead of setting an instance variable

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,18 +1,17 @@
 class Game < ApplicationRecord
   has_many :frames
 
-  # XXX move maximum Frame related code to Frame class
+  # XXX move Frame related code to Frame class
   def update_score(score)
     check_score(score)
     raise ArgumentError, 'Game is over' if self.is_over
     last_frame = self.frames.order(:number).last
-    @score = score
     if last_frame.nil?
-      create_frame
+      create_frame(nil, score)
     elsif !last_frame.is_over || (last_frame.is_over && last_frame.number == 10)
-      update_frame(last_frame)
+      update_frame(last_frame, score)
     else
-      create_frame(last_frame)
+      create_frame(last_frame, score)
     end
 
     self.is_over = game_over?(last_frame)
@@ -39,15 +38,15 @@ class Game < ApplicationRecord
 
   # This is a Factory method to create a frame and adjust scores
   # of the previous incomplete in terms of scoring frames
-  def create_frame(last_frame=nil)
+  def create_frame(last_frame=nil, score)
     number = 1
     if last_frame
-      last_frame.handle_spare(@score)
-      last_frame.handle_strike(@score)
+      last_frame.handle_spare(score)
+      last_frame.handle_strike(score)
       number += last_frame.number
     end
-    f = Frame.new(:game_id => self.id, :score => @score, :number => number)
-    if @score == 10
+    f = Frame.new(:game_id => self.id, :score => score, :number => number)
+    if score == 10
       f.is_over = true
       f.strike = true
     end
@@ -57,15 +56,15 @@ class Game < ApplicationRecord
   # Updates the frame in case of second turn inside one frame
   # or in case of additional throws after the spare or strike in the
   # last frame
-  def update_frame(last_frame)
+  def update_frame(last_frame, score)
     score_before_handle_strike = last_frame.score
-    last_frame.handle_strike(@score)
+    last_frame.handle_strike(score)
     if score_before_handle_strike == last_frame.score
-      if !last_frame.spare && !last_frame.strike && (last_frame.score + @score) > 10
+      if !last_frame.spare && !last_frame.strike && (last_frame.score + score) > 10
         raise ArgumentError, 'Impossible quantity of knocked down pins in '+
           "one frame"
       end
-      last_frame.score += @score
+      last_frame.score += score
     end
     if last_frame.spare && !last_frame.extra_turns_applied
       last_frame.extra_turns_applied = 1
